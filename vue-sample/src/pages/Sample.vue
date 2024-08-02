82c854793b96f19b48438edb8b67eb3c06e14a83
rngs <>
2024-08-03 00:03:27 +0900

    パンくず作成

--- vue-sample/src/pages/Sample.vue
+++ vue-sample/src/pages/Sample.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>Sample</div>
+  <div class="container">
+    <Breadcrumb :items="breadcrumb" />
+  </div>
 </template>
 
 <script>
+  import Breadcrumb from "./common/snipets/breadcrumb/Breadcrumb";
+
+  export default {
+    components: {
+      Breadcrumb
+    },
+    data() {
+      return {
+        breadcrumb: [
+          {
+            text: 'Admin',
+            href: '#'
+          },
+          {
+            text: 'Manage',
+            href: '#'
+          },
+          {
+            text: 'Library',
+            active: true
+          }
+        ]
+      }
+    }
+  };
 </script>
 
 <style>
